6b868a23965be5d85d6308736a8fd778f87c5c04
Michael <bencivma@clarkson.edu>
2022-10-03 19:46:53 -0400

    Finished MessageClackData w/o hashCode()

--- src/MessageClackData.java
+++ src/MessageClackData.java
@@ -15,9 +15,23 @@ public class MessageClackData extends ClackData {
         return this.message;
     }
 
-    //public int hashCode(){}
+//    public int hashCode(MessageClackData other){
+//     if (this.equals(other)){
+//         return ...
+//     }
+//     else{
+//         return ...
+//     }
+//    }
 
-    //public bool equals(){}
+    public boolean equals(MessageClackData other){
+        // if this doesn't work reference week 3 inheritance
+        // slide show slide 57, may need some sort of declaration
+        return this.getUserName() == other.getUserName() &&
+                this.getType() == other.getType() &&
+                this.getData() == other.getData() &&
+                this.message == other.message;
+    }
 
     public String toString(){
         return "The username is: " + this.getUserName() + "\n" +
