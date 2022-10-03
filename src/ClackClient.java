2c30f8ee6019340d409922e6235b83570d6a1b5e
LonerMon <114941130+LonerMon@users.noreply.github.com>
2022-10-03 18:31:40 -0400

    Add files via upload

---
+++ src/ClackClient.java
@@ -0,0 +1,10 @@
+//Java program for  the client of Clack
+
+public class ClackClient {
+
+    public static void main(String[] args){
+
+    }
+
+
+}
---
+++ src/ClackServer.java
@@ -0,0 +1,7 @@
+public class ClackServer {
+
+    public static void main(String[] args){
+
+        
+    }
+}
