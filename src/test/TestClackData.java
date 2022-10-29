ed1655929132bbda039594d9726a68869e3c7504
Michael <bencivma@clarkson.edu>
2022-10-28 21:20:27 -0400

    It is what it is :(

--- src/test/TestClackData.java
+++ src/test/TestClackData.java
@@ -6,6 +6,8 @@ import data.MessageClackData;
 
 public class TestClackData {
     public static void main (String args[]){
+
+        // Part 1 Test
         MessageClackData m1 = new MessageClackData("Michael", "Hello!", 2);
         MessageClackData m2 = new MessageClackData("Michael", "Hello!", 2);
         MessageClackData m3 = new MessageClackData();
@@ -47,5 +49,58 @@ public class TestClackData {
         System.out.println(f1.equals(f3));
 
         System.out.println(f1.toString());
+
+        // Part 2 Test
+        ClackData message1 = new MessageClackData("Michael","Hello World", 0);
+        ClackData message2 = new MessageClackData();
+        ClackData message3 = new MessageClackData("Johnny", "Goodbye World", -1);
+        ClackData message4 = new MessageClackData("Michael", 0, "TIME","BRAVENEWWORLD");
+
+        System.out.println("Message1: " + message1.getData());
+        System.out.println(message1.getType());
+
+        System.out.println(message2);
+        System.out.println(message2.getDate());
+
+        System.out.println(message1.equals(message2));
+
+        System.out.println(message3);
+        System.out.println(message3.hashCode());
+
+        System.out.println(message4.getData());
+
+        FileClackData file1 = new FileClackData("Michael", "projectFile", 0);
+        FileClackData file2 = new FileClackData();
+        FileClackData file3 = new FileClackData();
+        FileClackData file4 = new FileClackData("Johnny", "coolFile",0);
+        FileClackData file5 = new FileClackData("Dan","lameFile",0);
+
+        file2.setFileName("funFile");
+        System.out.println(file2);
+
+        file1.getFileName();
+        System.out.println("File 1: " + file1.getFileName());
+
+        System.out.println(file2.equals(file3));
+        System.out.println(file1.hashCode());
+        System.out.print(file2.hashCode());
+
+        System.out.println(file1.getType());
+        System.out.println(file1.getDate());
+
+        file4.readFileContents();
+        System.out.println(file4.getData());
+        file4.writeFileContents();
+        System.out.println(file4.getData());
+
+        file1.readFileContents();
+        file1.getData();
+        file1.writeFileContents();
+        System.out.println(file1.getData());
+
+
+        file5.readFileContents("time");
+        System.out.println(file5.getData());
+        file5.writeFileContents("time");
     }
 }
